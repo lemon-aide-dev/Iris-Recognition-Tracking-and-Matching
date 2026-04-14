89be01a33402e7c1ad02b5e085fcc3604e0b5990
Roy <royvincentdonila5@gmail.com>
2026-04-14 22:03:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from real_time_detection import live_iris_recognition
 
 # run live recognition
-live_iris_recognition("/home/iris-scanner/Desktop/Demo/core/dataset")
+live_iris_recognition("../dataset_path")
